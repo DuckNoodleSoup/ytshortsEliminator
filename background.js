a16174e61343ac8704f55b1f174642354f6dce79
Shuzo Katayama <shuzo.katayama@gmail.com>
2022-06-08 17:54:41 -0400

    video_id redirect added

--- background.js
+++ background.js
@@ -1,24 +1,31 @@
+function urlreplace(url) {
+    if (url.includes('shorts/')) {
+        url = url.replace('shorts/', 'watch?v=')
+    } else if (url.includes('video_id')) {
+        video_id = url.split('video_id=').pop().split('&')[0];
+        url = ("https://www.youtube.com/watch?v=" + video_id);
+        return url;
+    } 
+    return url;
+}
 
 const filter = {
     urls: [
+        '*://www.youtube.com/*',
         '*://www.youtube.com/shorts*'
     ],
 }
 
 const opt = ['blocking'];
 
-function urlreplace (url) {
-    if (url.includes('shorts/')) {
-        url = url.replace('shorts/', 'watch?v=')
-    }
-    return url;
-}
-
-//fct addListener(fct callback, var filter, var opt_extraInfoSpec);
+//ONBEFOREREQUEST
 window.chrome.webRequest.onBeforeRequest.addListener(
-    function callback (details) {
+    function callback(details) {
         url = details.url;
-        return {redirectUrl: urlreplace(url)}
+        if (url.includes('shorts/') || url.includes('video_id=')) {
+            updated_url = urlreplace(url)
+            return { redirectUrl: updated_url };
+        }
     },
     filter,
     opt
