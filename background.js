f812e66254e951a98979ec30ac37f69e4fa1749d
Shuzo Katayama <shuzo.katayama@gmail.com>
2022-06-10 20:54:01 -0400

    changed to chrome.tabs

--- background.js
+++ background.js
@@ -1,32 +1,17 @@
 function urlreplace(url) {
     if (url.includes('shorts/')) {
         url = url.replace('shorts/', 'watch?v=')
-    } else if (url.includes('video_id')) {
-        video_id = url.split('video_id=').pop().split('&')[0];
-        url = ("https://www.youtube.com/watch?v=" + video_id);
-        return url;
-    } 
+    }
     return url;
 }
 
-const filter = {
-    urls: [
-        '*://www.youtube.com/*',
-        '*://www.youtube.com/shorts*'
-    ],
-}
-
-const opt = ['blocking'];
-
-//ONBEFOREREQUEST
-window.chrome.webRequest.onBeforeRequest.addListener(
-    function callback(details) {
-        url = details.url;
-        if (url.includes('shorts/') || url.includes('video_id=')) {
+chrome.tabs.onUpdated.addListener(
+    async function callback(id, info, tab) {
+        url = tab.url;
+        if (url.includes('shorts/')) {
             updated_url = urlreplace(url)
-            return { redirectUrl: updated_url };
+            chrome.tabs.update(id, {url: updated_url});
         }
-    },
-    filter,
-    opt
-);
+        console.log(updated_url);
+    }
+)
